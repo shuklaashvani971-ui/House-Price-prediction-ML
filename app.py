104b09e28ed2409b878da9f6af8e21038f436637
shuklaashvani971-ui <shuklaashvani971@gmail.com>
2025-12-28 16:16:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ for i in X:
 scaler = StandardScaler()
 scaled_X = scaler.fit_transform(X)
 final_value = scaler.transform([all_value])
-model = RandomForestRegressor()
+model = st.cache(RandomForestRegressor)()
 model.fit(X,y)
 house_price = model.predict(final_value)[0]
 with st.spinner('predicting House price'):
@@ -34,6 +34,7 @@ msg = f'''house price is: $ {house_price*100000}'''
 st.success(msg)
 
 st.markdown('''**Design and Developed by: Ashvani shukla**''')
+
 
 
 
