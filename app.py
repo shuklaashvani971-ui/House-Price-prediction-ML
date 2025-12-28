32e2ea5c0731e54e4f963382651577763056442d
shuklaashvani971-ui <shuklaashvani971@gmail.com>
2025-12-28 15:17:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,12 +13,13 @@ y = df.iloc[:,-1]
 st.sidebar.title('🏘 Select House feature')
 st.image('https://cdn.dribbble.com/userupload/23217976/file/original-203940eb89dec42ee25f6af662af24dc.gif')
 all_value = []
-for i in X;
+for i in X:
   ans =  st.sidebar.slider(f'Select {i} value')
   all_value.append(ans)
 
 
 st.write(all_value)
+
 
 
 
