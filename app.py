f5f30707947b3d546dc0ee126e9ac09b9129d371
shuklaashvani971-ui <shuklaashvani971@gmail.com>
2025-12-28 16:20:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,10 +30,11 @@ model.fit(X,y)
 house_price = model.predict(final_value)[0]
 with st.spinner('predicting House price'):
   time.sleep(1)
-msg = f'''house price is: $ {house_price*100000}'''
+msg = f'''house price is: $ {round(house_price*100000,2)}'''
 st.success(msg)
 
 st.markdown('''**Design and Developed by: Ashvani shukla**''')
+
 
 
 
